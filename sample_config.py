70c28ea77e22b64a74cac0139a8f860995c5119e
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-04-21 11:43:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -35,7 +35,7 @@ class Config:
         "created using https://telegram.dog/UseTGxBot"
     )
     #
-    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @SpEcHlDe")
+    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @Alphaxhelpchat ❤️")
     # the strings used in the different messages
     # in the bot
     START_TEXT = os.environ.get("START_TEXT", Translation.START_TEXT)
