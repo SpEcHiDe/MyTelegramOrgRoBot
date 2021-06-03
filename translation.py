90d2032bdf2d7989830f1bd4d79da9ed2541c19f
sampathsir <83348785+sampathsir@users.noreply.github.com>
2021-06-03 19:13:24 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -17,4 +17,4 @@ by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pi
     ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+    IN_VALID_PHNO_PVDED = "You Phone Number is not Valid! It should be in international format"
