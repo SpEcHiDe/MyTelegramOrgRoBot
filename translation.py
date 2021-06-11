17c24dafe3e648ed2fc8268b6875ff17c78c83e0
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-06-11 20:02:52 +0530

    Update

--- translation.py
+++ translation.py
@@ -1,20 +1,17 @@
 class Translation(object):
     START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
-Thank you for using me 😬
-Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
+I am Disney Team Telegram org BOT I can get you your api hash and app id by my.telegram.org
 
-/start at any stage to re-enter your details"""
+/start again to get started 😊"""
     AFTER_RECVD_CODE_TEXT = """I see!
 now please send the Telegram code that you received from Telegram!
 
 this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
+Owner @doreamonfans1 and @doreamonfans2
 
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\@disneygrou"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
